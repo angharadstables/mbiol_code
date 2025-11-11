5bf733fbe9f846b0527f6dabfe0e8d1235f99cae
Angharad Stables <as3800@york.ac.uk>
2025-11-11 16:09:44 +0000

    Have continued working on the clustering analysis and have found where the NCOAs are clustered

--- dendrogram_script.R
+++ dendrogram_script.R
@@ -27,3 +27,29 @@ plot(hclust, labels = FALSE)
 
 # NCOA should be clustered together as they are nuclear receptor coactivator proteins for steroid receptors - GR
 # NCOR should also be clustered together- nuclear corepressor protein. 
+# Protein IDs: 
+# NCOA1   Q15788
+# NCOA2   Q15596
+# NCOA3   Q9Y6Q9
+# Creating a vector with just those in 
+NCOAs <- c("Q15788", "Q15596", "Q9Y6Q9")
+cluster40[NCOAs]
+# This tells us that Q15788 and Q15596, NCOA1 and NCOA2, are clustered together in cluster 21, and Q9Y6Q9, NCOA3, is clustered alone in cluster 7. 
+length(cluster40[cluster40 == 21])
+# There are 207 proteins in this cluster
+
+# Are they still clustered together when you cut it at 25?
+cluster25 <- cutree(hclust, h = 25)
+range(clusters_h25)
+# 63 clusters
+cluster25[NCOAs]
+# NCOA1 and 2 are still in the same cluster (cluster 39)
+length(cluster25[cluster25 == 39])
+# 139 proteins in this cluster
+# Investigating this cluster further: 
+# What are the other proteins in this cluster? 
+cluster39h25 <- names(cluster25[cluster25 == 39])
+write.csv(cluster39h25, file = "results/NCOA1-2_cluster.csv")
+
+# Where is the glucocorticoid receptor clustered?
+GR
