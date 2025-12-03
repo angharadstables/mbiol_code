8eef7fe870c6cec5935edf19e01d743e173b9689
Angharad Stables <as3800@york.ac.uk>
2025-12-03 09:36:39 +0000

    Beginning of my CSV enrichr analysis

---
+++ CSV enrichr analysis.R
@@ -0,0 +1,25 @@
+# CSV Enrichr Analysis 
+# Take the csv made in RStudio dendrogram code, then input it into DAVID, choosing that it is the uniprot accession ID and then changing it into the ‘official gene symbol’ which gives you the entrez gene name/symbol which can be put into enrichr. 
+# CSV 7
+# Pathways
+# Reactome - apoptotic execution phase
+# BioPlanet 2019 - pathways in cancer, small cell lung cancer, thyroid cancer, mrna splicing 
+# Ontologies
+# GO Biological Process 2025 -chromosome organisation ,reguoation of rna splicing, dna recombination
+# jensen diseases - lung squamous cell carcinoma
+# CSV 8
+# Pathways
+# Reactome pathways - RHO GTPase
+# Ontologies
+# GO Biological Processes - intermediate filament organisation, epithelium development, morphogenesis of epithelium, regulation of gene expression, positive regulation of transcription by RNA
+# CSV 9
+# Ontologies
+# GO Biological Processes - chromatin remodelling, cytoplasmic translation, DNA damage response, 
+# CSV 10
+# Pathways
+# Reactome pathways - RNA metabolism, cellular responses to stress, cell cycle, mitotitic,
+# Wikipathways - cytoplasmic ribosomal proteins, mRNA processing
+# Bioplanet - cytoplasmic ribosomal proteins, translation
+# Ontologies
+# GO Biological Processes - cytoplasmic translation, translation,
+# Diseases - cancer.
